2855ed173ea9181ec13738258f65d6824ae5dfdd
dmiramon <dmiramon@gmail.com>
2014-05-01 13:41:13 -0500

    Contact form testing
    Diego

--- Development/message.php
+++ Development/message.php
@@ -34,6 +34,6 @@ $email_body = "You have received a new message.\n\n" .
 $headers = "From: contact@mentorcliq.com\n";
 $headers .= "Reply-To: $contact-email"; 
 mail($to,$email_subject,$email_body,$headers);
-header('Location: /contact.html?send=true');
+header('Location: contact.html?send=true');
 die();
 ?>
